03bf0d63b4bd1066f887874928e24792a87911b9
Sherif Kamal <sherif.kamal.salem@gmail.com>
2019-01-24 06:32:27 +0200

    setup UI programmatically and animate card view

--- Tinsho/ViewController.swift
+++ Tinsho/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let topStackView = TopNavigationStackView()
+    let buttonsStackView = HomeBottomControlsStackView()
+    let cardDeckView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupLayout()
+        setupDummyCards()
     }
-
-
+    
+    //MARK: - Fileprivate
+    fileprivate func setupLayout() {
+        let overallStackView = UIStackView(arrangedSubviews: [topStackView, cardDeckView, buttonsStackView])
+        overallStackView.axis = .vertical
+        view.addSubview(overallStackView)
+        overallStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
+        overallStackView.isLayoutMarginsRelativeArrangement = true
+        overallStackView.layoutMargins = .init(top: 0, left: 12, bottom: 0, right: 12)
+        overallStackView.bringSubviewToFront(cardDeckView)
+    }
+    
+    // setup the animation of card
+    fileprivate func setupDummyCards() {
+        let cardView = CardView(frame: .zero)
+        cardDeckView.addSubview(cardView)
+        cardView.fillSuperview()
+    }
 }
-
